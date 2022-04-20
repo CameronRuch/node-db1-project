7d9a79f707a88667b301749ca447fba2c50ffdea
CameronRuch <cameron.ruch@yahoo.com>
2022-04-19 20:37:34 -0500

    all tests passing

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,45 +1,57 @@
 const router = require('express').Router()
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+const {
+  checkAccountId,
+  checkAccountNameUnique,
+  checkAccountPayload
+} = require('../accounts/accounts-middleware')
+
+router.get('/', async (req, res, next) => {
   try {
-    
+    const accounts = await Account.getAll()
+    res.json(accounts)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-
-  } catch (err) {
-    next(err)
-  }
+router.get('/:id', checkAccountId, async (req, res, next) => {
+  res.json(req.account)
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/',
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const newAccount = await Account.create({
+        name: req.body.name.trim(),
+        budget: req.body.budget
+      })
+      res.status(201).json(newAccount)
+    } catch (err) {
+      next(err)
+    }
+  })
+
+router.put('/:id',
+  checkAccountId,
+  checkAccountPayload,
+  checkAccountNameUnique,
+  async (req, res, next) => {
+    try {
+      const updated = await Account.updateById(req.params.id, req.body)
+      res.json(updated)
+    } catch (err) {
+      next(err)
+    }
+  });
+
+router.delete('/:id', checkAccountId, async (req, res, next) => {
   try {
-
-  } catch (err) {
-    next(err)
-  }
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-
-  } catch (err) {
-    next(err)
-  }
-});
-
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-  try {
-
+    await Account.deleteById(req.params.id)
+    res.json(req.account)
   } catch (err) {
     next(err)
   }
